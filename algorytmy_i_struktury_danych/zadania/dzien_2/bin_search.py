2537138b8feab0902d635182249c068f3a35ad5f
lucaspal <paluchagh@gmail.com>
2020-06-22 20:03:12 +0200

    homework 1

--- algorytmy_i_struktury_danych/zadania/dzien_2/bin_search.py
+++ algorytmy_i_struktury_danych/zadania/dzien_2/bin_search.py
@@ -1,3 +1,40 @@
 """
     implementacja wyszukiwania binarnego
 """
+
+import time
+
+
+def bin_search(a: list, x: int) -> str:
+
+    l = 0
+    p = len(a) - 1
+
+    while not l > p:  # l <= p
+
+        s = (l+p)//2
+
+        if a[s] == x:
+            return f"odnaleziono element {x}, pod indeksem {s}"
+
+        if a[s] < x:
+            l = s + 1
+        else:
+            p = s - 1
+
+    return f"nie odnaleziono elementu o wartosci {x}"
+
+
+def find(a: list, x: int) -> str:
+    for element in a:
+        if element == x:
+            return "znaleziono"
+    return "nie znaleziono"
+
+
+a = time.perf_counter()
+print(bin_search([num for num in range(100000)], 99999))
+b = time.perf_counter()
+print(find([num for num in range(100000)], 99999))
+c = time.perf_counter()
+print(f"czas bin search: {b-a}, czas norm: {c-b}")
